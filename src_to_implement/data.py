e4443d89b7fa954e764876573a028b538fcad2f7
sumit aswal <sumit.aswal@magicpin.in>
2024-07-13 17:02:57 +0200

    data.py

--- src_to_implement/data.py
+++ src_to_implement/data.py
@@ -11,5 +11,38 @@ train_std = [0.16043035, 0.16043035, 0.16043035]
 
 
 class ChallengeDataset(Dataset):
-    # TODO implement the Dataset class according to the description
-    pass
+    def __init__(self, data, mode='train'):
+        self.data = data
+        self.mode = mode
+
+        if self.mode == 'train':
+            self._transform = tv.transforms.Compose([
+                tv.transforms.ToPILImage(),
+                tv.transforms.RandomHorizontalFlip(),
+                tv.transforms.RandomVerticalFlip(),
+                tv.transforms.RandomRotation(10),
+                tv.transforms.RandomCrop(256, padding=4),
+                tv.transforms.ToTensor(),
+                tv.transforms.Normalize(mean=train_mean, std=train_std)
+            ])
+        else:
+            self._transform = tv.transforms.Compose([
+                tv.transforms.ToPILImage(),
+                tv.transforms.ToTensor(),
+                tv.transforms.Normalize(mean=train_mean, std=train_std)
+            ])
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img_path = Path(self.data.iloc[idx, 0])
+        image = imread(img_path)
+        image = gray2rgb(image)  # Convert grayscale to RGB
+        label = self.data.iloc[idx, 1:].values.astype('float')
+        label = torch.tensor(label, dtype=torch.float32)
+
+        if self._transform:
+            image = self._transform(image)
+
+        return image, label
